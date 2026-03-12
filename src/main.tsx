9e93b6c859acbcdf1a89f1f281694c2a7e024ddc
dralmarri <prof.almarri@gmail.com>
2026-03-12 21:32:15 +0300

    Initialize store before rendering the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import ErrorBoundary from "./components/ErrorBoundary";
 import App from "./App.tsx";
 import "./index.css";
+import { initStore } from "@/lib/store";
 
-createRoot(document.getElementById("root")!).render(
-  <ErrorBoundary>
-    <App />
+initStore().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  );
+});
   </ErrorBoundary>
 );
